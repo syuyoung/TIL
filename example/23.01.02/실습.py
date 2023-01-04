8200f038da7162bb5685eeecb281857c40b028cf
syyoung <yuyoung.dev@gmail.com>
2023-01-04 13:31:14 +0900

    write

---
+++ example/23.01.02/실습.py
@@ -0,0 +1,65 @@
+number = int(input("숫자를 입력해주세요 > "))
+print(number + 10)
+
+
+name = input("좋아하는 음식을 입력해주세요 > ")
+print("좋아하는 음식 : ",name)
+
+
+name = input("이름을 입력해주세요 > ")
+year = int(input("태어난 년도를 입력해주세요 > "))
+
+print("저의 이름은", name, "이고, 올해 나이는 ", 2024 - year, "세 입니다")
+
+
+word1 = input("첫 번째 문장을 입력해주세요 > ")
+word2 = input("두 번째 문장을 입력해주세요 > ")
+
+print(word1 + word2)
+
+
+number = int(input("숫자를 입력해주세요 > "))
+print(-number)
+
+
+number1 = int(input("첫 번째 숫자를 입력해주세요 > "))
+number2 = int(input("두 번째 숫자를 입력해주세요 > "))
+
+print("더하기 연산 : ",number1 + number2)
+print("빼기 연산 : ",number1 - number2)
+print("곱하기 연산 : ",number1 * number2)
+print("몫 연산 : ",number1 // number2)
+print("나머지 연산 : ",number1 % number2)
+
+
+number1 = int(input("첫 번째 숫자를 입력해주세요 > "))
+number2 = int(input("두 번째 숫자를 입력해주세요 > "))
+number3 = int(input("세 번째 숫자를 입력해주세요 > "))
+
+print((number1 + number2 + number3) /3)
+
+
+number1 = int(input("첫 번째 숫자를 입력해주세요 > "))
+number2 = int(input("두 번째 숫자를 입력해주세요 > "))
+number3 = int(input("세 번째 숫자를 입력해주세요 > "))
+number4 = int(input("네 번째 숫자를 입력해주세요 > "))
+number5 = int(input("다섯 번째 숫자를 입력해주세요 > "))
+print([number1, number2, number3, number4, number5])
+
+
+word = input("문자열을 입력해주세요 > ")
+number = int(input("숫자를 입력해주세요 > "))
+
+print(word * number)
+
+
+number1 = int(input("첫 번째 숫자를 입력해주세요 > "))
+print(number1)
+number2 = int(input("두 번째 숫자를 입력해주세요 > "))
+print(number1 + number2)
+number3 = int(input("세 번째 숫자를 입력해주세요 > "))
+print(number1 + number2 + number3)
+number4 = int(input("네 번째 숫자를 입력해주세요 > "))
+print(number1 + number2 + number3 + number4)
+number5 = int(input("다섯 번째 숫자를 입력해주세요 > "))
+print(number1 + number2 + number3 + number4 + number5)
