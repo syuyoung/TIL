663bc5f3257fff56422781deaecf5a91bad09174
syyoung <yuyoung.dev@gmail.com>
2023-01-10 13:41:35 +0900

    write

---
+++ example/23.01.10/fun.py
@@ -0,0 +1,3 @@
+def cube(a):
+    return a**3
+print(cube(5))
---
+++ example/23.01.10/iopractice.py
@@ -0,0 +1,49 @@
+# 1
+
+num = [5]
+print(num)
+
+
+# 2
+
+word = 'hello python word'
+print(word)
+
+
+# 3
+
+num = [1,2,3]
+for i in num:
+    print(i)
+
+
+# 4
+
+num = [2,0,3,92,3]
+for i in num:
+    print(i, end=' ')
+
+
+# 5
+num = '2'' ''3'
+print(num)
+
+
+# 6
+
+word = ['I','am','Programmer']
+for i in word:
+    print(i,end=' ')
+
+
+# 7
+
+num = input()
+for i in num:
+    print(i, end=' ')
+
+
+# 8
+
+num = input()
+print(num)
