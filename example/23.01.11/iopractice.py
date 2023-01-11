8c9fd35d93a668be756bbfc2a95a77945185de82
syyoung <yuyoung.dev@gmail.com>
2023-01-11 14:34:16 +0900

    write

---
+++ example/23.01.11/iopractice.py
@@ -0,0 +1,103 @@
+# 1
+
+numbers = list(map(int,input().split()))
+
+print(numbers)
+
+
+# 2
+
+string = input().split()
+
+print(string)
+
+
+# 3
+
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        num += 1
+        print(num)
+
+
+# 4
+
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        a, b = list(map(int,input().split()))
+
+print(a, b)
+
+
+5
+
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        string = input().split()
+        print(string)
+        
+
+# 6
+
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        numbers = list(map(int,input().split()))
+
+print(numbers)
+
+
+# 7
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        print(t)
+
+
+# 8
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        a, b = list(map(int,input().split()))
+
+print(a, b)
+
+
+# 9
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = int(input())
+
+    for num in range(N):
+        numbers = list(map(int,input().split()))
+
+print(numbers)
