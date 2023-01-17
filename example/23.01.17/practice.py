d7c5da6812d0264f9aa928d4f85b9bae0771c4e4
syyoung <yuyoung.dev@gmail.com>
2023-01-17 16:34:15 +0900

    write

---
+++ example/23.01.17/practice.py
@@ -0,0 +1,49 @@
+# # 9085
+
+# T = int(input())
+
+# for t in range(1, T+1):
+#     N = input()
+#     numbers = list(map(int,input().split()))
+#     print(sum(numbers))
+
+# # 10824
+
+# a,b,c,d = map(str,input().split())
+# ab = int(a+b)
+# cd = int(c+d)
+# print(ab+cd)
+
+# # 3009
+
+# x1, y1 = map(int, input().split())
+# x2, y2 = map(int, input().split())
+# x3, y3 = map(int, input().split())
+# x4 = 0
+# y4 = 0
+# if x1 == x2:
+#     x4 = x3
+# elif x1 == x3:
+#     x4 = x2
+# else:
+#     x4 = x1
+
+# if y1 == y2:
+#     y4 = y3
+# elif y1 == y3:
+#     y4 = y2
+# else:
+#     y4 = y1
+
+# print(x4,y4)
+
+# # 10952
+# while True:
+#     a, b = map(int,input().split())
+#     if a+b > 0:
+#         print(a+b)
+#     else:
+#         break
+
+## 1110
+
