c79aa0c395bd2c920f6178a676ae6fad3c939265
syyoung <yuyoung.dev@gmail.com>
2023-01-26 17:46:49 +0900

    write

--- example/1-4/23.1.26/a.py
+++ example/1-4/23.1.26/a.py
@@ -1,6 +1,20 @@
-n = int(input())
-card = list(range(1,n+1))
-while len(card) > 1:
-    print(card.pop(0), end=' ')
-    card.append(card.pop(0))
-print(card[0])
+# n = int(input())
+# card = list(range(1,n+1))
+# while len(card) > 1:
+#     print(card.pop(0), end=' ')
+#     card.append(card.pop(0))
+# print(card[0])
+
+import sys
+input = sys.stdin.readline()
+
+K = int(input)
+stack = []
+
+for i in range(K):
+    num = int(input)
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+print(sum(stack))
