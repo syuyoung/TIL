fe519b2f0a9ad28021f41d33ff79f9adf777fb47
syyoung <yuyoung.dev@gmail.com>
2023-02-02 17:16:04 +0900

    write

---
+++ example/2-1/23.2.2/practice.py
@@ -0,0 +1,48 @@
+# # 1547
+
+# import sys
+# input = sys.stdin.readline
+
+# ball = 1
+# for _ in range(int(input())):
+#     a, b = map(int, input().split())
+#     if a == ball:
+#         ball = b
+#     elif b == ball:
+#         ball = a
+# print(ball)
+
+# # 5576
+
+# import sys
+# sys.stdin = open("input.txt", "r")
+
+# input = sys.stdin.readline
+
+# w = [int(input()) for _ in range(10)]
+# w = sorted(w)
+# k = [int(input()) for _ in range(10)]
+# k = sorted(k)
+# print(sum(w[-3:]), sum(k[-3:]))
+
+# 2846
+
+import sys
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+n = int(input())
+way = list(map(int, input().split()))
+num = 0
+result = []
+#1 2 1 4 6
+for i in range(n-1):
+    if way[i] < way[i+1]:
+        num += way[i+1] - way[i]
+
+    elif way[i] >= way[i+1]:
+        result.append(num)
+        num = 0
+    
+result.append(num)
+print(max(result))
