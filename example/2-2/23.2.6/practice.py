a04ee78d240e30bc2c443d9b32c6e2bd13be7a39
syyoung <yuyoung.dev@gmail.com>
2023-02-06 20:30:34 +0900

    write

---
+++ example/2-2/23.2.6/practice.py
@@ -0,0 +1,31 @@
+# #10769
+
+# word = input()
+# happy = word.count(':-)')
+# sad = word.count(':-(')
+
+
+# if happy == 0 and sad == 0:
+#     print('none')
+# elif happy == sad:
+#     print('unsure')
+# elif happy > sad:
+#     print('happy')
+# elif happy < sad:
+#     print('sad')
+
+
+import sys
+sys.stdin = open("input.txt", "r")
+
+people = []
+result = 0
+
+for _ in range(4):
+    a, b = map(int,sys.stdin.readline().split())
+    result += b
+    result -= a
+
+    people.append(result)
+
+print(max(people))
