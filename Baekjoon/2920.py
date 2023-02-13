2697fcb2f7d955ab24e63fd892fff613e20ae511
syyoung <yuyoung.dev@gmail.com>
2023-02-13 17:19:40 +0900

    write

---
+++ Baekjoon/2920.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = list(map(int,input().split()))
+if N == sorted(N):
+    print('ascending')
+elif N == sorted(N, reverse=True):
+    print('descending')
+else:
+    print('mixed')
