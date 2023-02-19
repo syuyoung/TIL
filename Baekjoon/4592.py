5f40e191c37c104fe21cdfb522408b1418b650a3
syyoung <yuyoung.dev@gmail.com>
2023-02-19 23:24:32 +0900

    write

---
+++ Baekjoon/4592.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+input = sys.stdin.readline
+while True:
+    li = []
+    arr = ''
+    number = input().split()
+    if number[0] == '0':
+        break
+    for i in number:
+        if i not in li:
+            li.append(i)
+        elif i in li:
+            if li[-1] != i:
+                li.append(i)
+    print(*li[1::], '$')
