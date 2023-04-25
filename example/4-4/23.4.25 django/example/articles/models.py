84d592ae719cec979db508dc2816812b557e5f33
syyoung <yuyoung.dev@gmail.com>
2023-04-25 18:08:02 +0900

    write

---
+++ example/4-4/23.4.25 django/example/articles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
