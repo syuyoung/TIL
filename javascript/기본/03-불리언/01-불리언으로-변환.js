165c5a1bc7f2cd5eb618228601944ebb27d0b8f1
syyoung <yuyoung.dev@gmail.com>
2023-07-31 22:53:20 +0900

    write

---
+++ javascript/기본/03-불리언/01-불리언으로-변환.js
@@ -0,0 +1,16 @@
+/**
+ * Boolean
+ *
+ * ON: True
+ * OFF: False
+ */
+console.log(Boolean('STRING'));
+console.log(!!'STRING');
+console.log(!!0);
+console.log(!!1);
+
+if ( ) {
+  // some code
+} else {
+  // some code
+}
